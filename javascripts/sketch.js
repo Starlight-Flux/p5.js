5cd3f69151f8ffd293f5b8bc272bdfe618640217
Starlight-Flux <Starlight.Flux@gmail.com>
2015-01-19 15:29:17 +0000

    Update sketch.js

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
   // put setup code here
+  var myCanvas = createCanvas(600, 400);
+  myCanvas.parent('myContainer');
 }
 
 function draw() {
   // put drawing code here
+  ellipse(width/2, height/2, 50, 50);
 }
